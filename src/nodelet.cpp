7e831a8f7694337a51ed07284341c222cef3560b
fxprime <thanabadee_bc@hotmail.com>
2020-06-05 18:16:52 +0700

    Fix subscribe update

--- src/nodelet.cpp
+++ src/nodelet.cpp
@@ -20,17 +20,13 @@ const float target_quid = 33;
 void openSerial(const string& sport);
 void cmdCb(const geometry_msgs::Twist::ConstPtr &msg) 
 {
-    ros::Time t_now                 = ros::Time::now();
-    static ros::Time last_send_cmd  = t_now;
-    if( t_now - last_send_cmd > ros::Duration(0.1) ) {
-        last_send_cmd = t_now;
-        cnt_status_s pmsg;
-        pmsg.vel_cnt.vx = msg->linear.x*100;
-        pmsg.vel_cnt.vy = msg->linear.y*100;
-        pmsg.vel_cnt.wz = msg->angular.z*100;
-        send_vel_cmd_status(target_quid, pmsg);
-        ROS_INFO("Received cmd_vel %.2f %.2f %.2f", msg->linear.x, msg->linear.y, msg->angular.z);
-    }
+    cnt_status_s pmsg;
+    pmsg.vel_cnt.vx = msg->linear.x*100;
+    pmsg.vel_cnt.vy = msg->linear.y*100;
+    pmsg.vel_cnt.wz = msg->angular.z*100;
+    send_vel_cmd_status(target_quid, pmsg);
+    ROS_INFO("Received cmd_vel %.2f %.2f %.2f", msg->linear.x, msg->linear.y, msg->angular.z);
+    
 }
 
 
@@ -48,7 +44,7 @@ int main(int argc, char *argv[]) {
     _imu_pub          = n.advertise<sensor_msgs::Imu>("car_connect/imu",               1);
     _odom_pub         = n.advertise<nav_msgs::Odometry>("car_connect/odom",            1);
     _vel_cmd_pub      = n.advertise<geometry_msgs::Twist>("car_connect/vel_cmd",       1);
-    ros::Subscriber sub_cmd_vel = n.subscribe("/cmd_vel", 1, &cmdCb);
+    ros::Subscriber sub_cmd_vel = n.subscribe("/cmd_vel", 10, cmdCb);
 
     while(ros::ok()) {
 
@@ -76,6 +72,7 @@ int main(int argc, char *argv[]) {
 
 
         usleep(1);
+        ros::spinOnce();
     }
 
 }
